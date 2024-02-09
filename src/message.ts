d2258f6307cbc0bbaf737d027c554c27a45e7b9c
VelislavKarastoychev <exel_mmm@abv.bg>
2024-02-09 18:17:02 +0200

    Added js extensions.

--- src/message.ts
+++ src/message.ts
@@ -1,5 +1,5 @@
 "use strict";
-import { ColorsType, ColorType, Integer } from "./types";
+import { ColorsType, ColorType, Integer } from "./types.ts";
 const author: string = "Velislav S. Karastoychev";
 const version: string = "1.0.0";
 const regexTest: RegExp =
